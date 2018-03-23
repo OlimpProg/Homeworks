dce6656576a6caf27fadb93c371f1b2e87d84e3a
kuzandro <kuzandro@gmail.com>
2018-03-23 19:17:06 +0300

    Added functions by kuzandro

---
+++ sorting_n-2/kuzandro/swap.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "header.h"
+
+using namespace std;
+
+void Swap(int *one, int *two){
+    int temp = *one;
+    *one = *two;
+    *two = temp;
+
+}
